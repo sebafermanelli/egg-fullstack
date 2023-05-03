07c270f4e5b960dd730b30129ef51f8bd3001445
Sebastian Fermanelli <sebastianfermanelli@gmail.com>
2023-05-03 16:15:23 -0300

    Actualizacion Encuentro 32

---
+++ src/guia9/extras/ejercicio1/Core.java
@@ -0,0 +1,26 @@
+package guia9.extras.ejercicio1;
+
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
+public class Core {
+    public static void main(String[] args) {
+        String[] meses = new String[]{"enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+        String mesSecreto = meses[9];
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adivine el mes secreto");
+        System.out.print("Introduzca el nombre del mes: ");
+        String mesIngresado = scanner.nextLine().toLowerCase();
+
+        while (!mesIngresado.equals(mesSecreto)) {
+            System.out.println("Incorrecto! Te doy una pista: comienza con " +  mesSecreto.charAt(0));
+            System.out.print("Introduzca el nombre del mes nuevamente: ");
+            mesIngresado = scanner.nextLine();
+        }
+
+        System.out.println("Adivinaste! El mes era " + mesSecreto);
+    }
+}
